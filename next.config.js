e1bb7442a2b1a2435eb6525b7e8cb1a22bc43c30
Saff9 <saffanakbar942@gmail.com>
2025-11-20 20:12:18 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const withPWA = require('@next/pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['openweathermap.org'],
+  },
+};
+
+module.exports = withPWA(nextConfig);
