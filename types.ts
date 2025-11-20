79f02d2e0d9773d2464a4e087e131fb1f31b4fdc
Saff9 <saffanakbar942@gmail.com>
2025-11-20 20:40:16 +0530

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,23 @@
+// types.ts
+export interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+  };
+  weather: Array<{
+    main: string;
+    description: string;
+    icon: string;
+  }>;
+  wind: {
+    speed: number;
+  };
+}
+
+export interface MenuProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
