8d78047fa9499abfc9f61199cbe3f798dbb3ebfe
Saff9 <saffanakbar942@gmail.com>
2025-11-21 10:40:58 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import './globals.css'
 import { Inter } from 'next/font/google'
+import ThemeInitializer from '../components/ThemeInitializer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,13 +18,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="theme-color" content="#FFFFFF" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>
+        <ThemeInitializer />
         {children}
         
         {/* Service Worker Registration */}
